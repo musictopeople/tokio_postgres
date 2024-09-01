c22b3273930b773d9c8bc93c3424804807de436d
Cameron <cameron@pop-os.localdomain>
2024-09-01 19:30:24 -0400

    adding constants file

---
+++ src/constants.rs
@@ -0,0 +1,2 @@
+pub const SELECT_POST_BY_ID: &str = "select body from posts where id = $1";
+pub const DATABASE_CONNECTION_ERROR: &str = "DATABASE_URL environment variable not set";
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use dotenv::dotenv;
 use tokio_postgres::{Error, NoTls};
 
+mod constants;
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     // load environment variables from `.env` file
     dotenv().ok();
     // set temporary variable to connect db using url
-    let database_url =
-        std::env::var("DATABASE_URL").expect("DATABASE_URL environment variable not set");
+    let database_url = std::env::var("DATABASE_URL").expect(constants::DATABASE_CONNECTION_ERROR);
     // set url for postgres db
     let (client, connection) = tokio_postgres::connect(&database_url, NoTls).await?;
 
@@ -17,9 +18,7 @@ async fn main() -> Result<(), Error> {
         }
     });
 
-    let rows = client
-        .query("select body from posts where id = $1", &[&1])
-        .await?;
+    let rows = client.query(constants::SELECT_POST_BY_ID, &[&1]).await?;
 
     let value: &str = rows[0].get(0);
     println!("\n{}!!!!!\n", value); // Create a new Actix Web application
